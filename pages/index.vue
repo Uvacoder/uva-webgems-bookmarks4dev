947ce73d0e8738bc21d9d091bee3e64c8f69f3d6
Uvacoder <uva22940coder@gmail.com>
2022-09-10 13:49:20 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
 .welcome
   main
-    h1.text-black.font-bold.text-xl.mb-2 What is&nbsp;
-      span 💎 webgems?
-    p webgems is a goto place for devs and designers to find new resources and more. As a beginner, it's sometimes not easy to find what you need since you don't know where to look for what.
+    h1.text-black.font-bold.text-xl.mb-2 What are&nbsp;
+      span 💎 abcLinks?
+    p abcLinks is a goto place for devs and designers to find new resources and more. As a beginner, it's sometimes not easy to find what you need since you don't know where to look for what.
     p Therefore we created this project to have the most common and best resources for beginners and veterans in one place — kind of like a bookmarks list for devs and designers, containing only the best gems out there.
     p If you want to add your gem, go ahead and open a pull request on GitHub (click on that octocat at the top right corner).
     p By the way, this project lives by getting shared; if you find this worthy, please share it with others.
     
     p.muted // made with love by
     p.contributors class&nbsp;
-      a(href="https://twitter.com/AlecCam43544378") @AlecCam43544378&nbsp;
+      a(href="https://twitter.com/VaLandscapes") @VaLandscapes&nbsp;
       span extends&nbsp;
       a(href="https://github.com/uvacoder") @uvacoder&nbsp;
       span and&nbsp;
